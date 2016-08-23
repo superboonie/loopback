d1b198715118183b66185c649329e5847077ed23
Loay <loay@users.noreply.github.com>
2016-08-23 11:01:59 -0400

    Fix password expiry session

--- common/models/user.js
+++ common/models/user.js
@@ -568,6 +568,17 @@ module.exports = function(User) {
         err.code = 'EMAIL_NOT_FOUND';
         return cb(err);
       }
+
+      UserModel.findById({ where: { email: options.email }}, function(err, accessToken) {
+        if (err) {
+          fn(err);
+        } else if (accessToken) {
+          accessToken.destroy(fn);
+        } else {
+          cb(new Error(g.f('could not find {{accessToken}}')));
+        }
+      });
+
       // create a short lived access token for temp login to change password
       // TODO(ritch) - eventually this should only allow password change
       if (UserModel.settings.emailVerificationRequired && !user.emailVerified) {
